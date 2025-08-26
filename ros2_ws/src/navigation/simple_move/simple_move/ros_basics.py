a166a3289017f71c6165c01eb8ff32d155ac53a4
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2025-08-25 23:56:16 -0600

    Updates for a02

--- ros2_ws/src/navigation/simple_move/simple_move/ros_basics.py
+++ ros2_ws/src/navigation/simple_move/simple_move/ros_basics.py
@@ -1,11 +1,22 @@
+#
+# MOBILE ROBOTS - FI-UNAM, 2026-1
+# THE PLATFORM ROS 
+#
+# Instructions:
+# Write a program to move the robot forwards until the laser
+# detects an obstacle in front of it.
+# Required publishers and subscribers are already declared and initialized.
+
 import rclpy
 from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
+FULL_NAME = "FULL NAME"
+
 class RosBasicsNode(Node):
     def __init__(self):
-        print("INITIALIZING SIMPLE MOVE NODE ...")
+        print("INITIALIZING SIMPLE MOVE NODE - ", FULL_NAME)
         super().__init__("ros_basics_node")
         self.pub_cmd_vel = self.create_publisher(Twist, '/cmd_vel', 1)
         self.sub_scan = self.create_subscription(LaserScan, '/scan', self.callback_scan, 1)
@@ -13,12 +24,23 @@ class RosBasicsNode(Node):
         self.obstacle_detected = False
 
     def callback_timer(self):
-        msg = Twist()
-        msg.linear.x = 0.0 if self.obstacle_detected else 0.3
-        self.pub_cmd_vel.publish(msg)
+        #
+        # TODO:
+        # Declare a Twist message and assign the appropiate speeds:
+        # Move forward if there is no obstacle in front of the robot, and stop otherwise.
+        # Use the 'obstacle_detected' variable to check if there is an obstacle. 
+        # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
+        
+        return
 
     def callback_scan(self, msg):
-        self.obstacle_detected = msg.ranges[len(msg.ranges)//2] < 1.0
+        #
+        # TODO:
+        # Do something to detect if there is an obstacle in front of the robot.
+        # Set the 'obstacle_detected' variable with True or False, accordingly.
+        #
+        
+        return
 
 
 def main(args=None):
