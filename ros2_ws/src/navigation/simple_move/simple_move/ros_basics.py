6a13ee9bfc288c7830f55ac1835bfacff253bce0
quarcks1 <axeldeck@icloud.com>
2025-09-01 12:42:41 -0600

    Tarea02

--- ros2_ws/src/navigation/simple_move/simple_move/ros_basics.py
+++ ros2_ws/src/navigation/simple_move/simple_move/ros_basics.py
@@ -12,7 +12,7 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import LaserScan
 
-FULL_NAME = "FULL NAME"
+FULL_NAME = "AXEL FERNANDO MENDOZA FLORES"
 
 class RosBasicsNode(Node):
     def __init__(self):
@@ -30,7 +30,9 @@ class RosBasicsNode(Node):
         # Move forward if there is no obstacle in front of the robot, and stop otherwise.
         # Use the 'obstacle_detected' variable to check if there is an obstacle. 
         # Publish the Twist message using the already declared publisher 'pub_cmd_vel'.
-        
+        msg = Twist()
+        msg.linear.x = 0.0 if self.obstacle_detected else 0.3
+        self.pub_cmd_vel.publish(msg)
         return
 
     def callback_scan(self, msg):
@@ -38,8 +40,7 @@ class RosBasicsNode(Node):
         # TODO:
         # Do something to detect if there is an obstacle in front of the robot.
         # Set the 'obstacle_detected' variable with True or False, accordingly.
-        #
-        
+        self.obstacle_detected = msg.ranges[len(msg.ranges)//2] < 1.0
         return
 
 
