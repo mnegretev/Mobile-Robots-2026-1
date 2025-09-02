01769c06bc0cfe7b46b434803f970eff6936e55b
christian-pineda <krictian1103@gmail.com>
2025-09-02 02:04:28 -0600

    comentario

---
+++ build/xarm_controller/colcon_command_prefix_build.sh
@@ -0,0 +1,5 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/christian-pineda/Mobile-Robots-2026-1/install/xarm_description/share/xarm_description/package.sh"
+. "/home/christian-pineda/Mobile-Robots-2026-1/install/xarm_msgs/share/xarm_msgs/package.sh"
+. "/home/christian-pineda/Mobile-Robots-2026-1/install/xarm_sdk/share/xarm_sdk/package.sh"
+. "/home/christian-pineda/Mobile-Robots-2026-1/install/xarm_api/share/xarm_api/package.sh"
---
+++ build/xarm_sdk/CMakeFiles/xarm_cxx_sdk.dir/cxx/src/serial/impl/win.cc.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/xarm_cxx_sdk.dir/cxx/src/serial/impl/win.cc.o: \
+ /home/christian-pineda/Mobile-Robots-2026-1/ros2_ws/src/hardware/xarm_ros2/xarm_sdk/cxx/src/serial/impl/win.cc \
+ /usr/include/stdc-predef.h
