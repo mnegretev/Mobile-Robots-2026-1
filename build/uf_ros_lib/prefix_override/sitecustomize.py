f20cba7eb89c3d510da04e48978c564f49d0b796
AxelJRM <theaxelruiz@gmail.com>
2025-09-02 18:22:09 -0600

    Elimino carpetas build, install y log

--- build/uf_ros_lib/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/axel/Mobile-Robots-2026-1/install/uf_ros_lib'
--- build/xarm_api/colcon_command_prefix_build.sh
+++
@@ -1,4 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/axel/Mobile-Robots-2026-1/install/xarm_description/share/xarm_description/package.sh"
-. "/home/axel/Mobile-Robots-2026-1/install/xarm_msgs/share/xarm_msgs/package.sh"
-. "/home/axel/Mobile-Robots-2026-1/install/xarm_sdk/share/xarm_sdk/package.sh"
