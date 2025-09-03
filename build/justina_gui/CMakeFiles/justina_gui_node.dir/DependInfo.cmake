090d40ebb04fea1b3847c92d79ec108be7070452
christosanj <christopher.sanjuan.flores@gmail.com>
2025-09-02 21:43:43 -0600

    Tarea 02

---
+++ build/justina_gui/CMakeFiles/justina_gui_node.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "" "justina_gui_node_autogen/timestamp" "custom" "justina_gui_node_autogen/deps"
+  "/home/christopher/Mobile-Robots-2026-1/build/justina_gui/include/moc_mainwindow.cpp" "CMakeFiles/justina_gui_node.dir/include/moc_mainwindow.cpp.o" "gcc" "CMakeFiles/justina_gui_node.dir/include/moc_mainwindow.cpp.o.d"
+  "/home/christopher/Mobile-Robots-2026-1/build/justina_gui/include/moc_rclcomm.cpp" "CMakeFiles/justina_gui_node.dir/include/moc_rclcomm.cpp.o" "gcc" "CMakeFiles/justina_gui_node.dir/include/moc_rclcomm.cpp.o.d"
+  "/home/christopher/Mobile-Robots-2026-1/build/justina_gui/justina_gui_node_autogen/mocs_compilation.cpp" "CMakeFiles/justina_gui_node.dir/justina_gui_node_autogen/mocs_compilation.cpp.o" "gcc" "CMakeFiles/justina_gui_node.dir/justina_gui_node_autogen/mocs_compilation.cpp.o.d"
+  "/home/christopher/Mobile-Robots-2026-1/build/justina_gui/qrc_resource.cpp" "CMakeFiles/justina_gui_node.dir/qrc_resource.cpp.o" "gcc" "CMakeFiles/justina_gui_node.dir/qrc_resource.cpp.o.d"
+  "/home/christopher/Mobile-Robots-2026-1/ros2_ws/src/hri/justina_gui/src/justina_gui_node.cpp" "CMakeFiles/justina_gui_node.dir/src/justina_gui_node.cpp.o" "gcc" "CMakeFiles/justina_gui_node.dir/src/justina_gui_node.cpp.o.d"
+  "/home/christopher/Mobile-Robots-2026-1/ros2_ws/src/hri/justina_gui/src/mainwindow.cpp" "CMakeFiles/justina_gui_node.dir/src/mainwindow.cpp.o" "gcc" "CMakeFiles/justina_gui_node.dir/src/mainwindow.cpp.o.d"
+  "/home/christopher/Mobile-Robots-2026-1/ros2_ws/src/hri/justina_gui/src/rclcomm.cpp" "CMakeFiles/justina_gui_node.dir/src/rclcomm.cpp.o" "gcc" "CMakeFiles/justina_gui_node.dir/src/rclcomm.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/xarm_msgs/xarm_msgs__py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/christopher/Mobile-Robots-2026-1/build/xarm_msgs/xarm_msgs__py
+# Build directory: /home/christopher/Mobile-Robots-2026-1/build/xarm_msgs/xarm_msgs__py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
