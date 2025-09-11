7a0db5a9fbb3f9376645f3338349e29fe3f59dfb
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2025-09-11 15:16:01 -0600

    Add files for path planning

---
+++ ros2_ws/src/navigation/path_planner/setup.py
@@ -0,0 +1,31 @@
+from setuptools import find_packages, setup
+
+package_name = 'path_planner'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='thedoctor',
+    maintainer_email='marco.negrete@ingenieria.unam.edu',
+    description='TODO: Package description',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'rrt = path_planner.rrt:main',
+            'rrt_solved = path_planner.rrt_solved:main',
+            'cost_map = path_planner.cost_map:main',
+            'cost_map_solved = path_planner.cost_map_solved:main',
+            'a_start = path_planner.a_star:main',
+            'a_start_solved = path_planner.a_star_solved:main',
+        ],
+    },
+)
