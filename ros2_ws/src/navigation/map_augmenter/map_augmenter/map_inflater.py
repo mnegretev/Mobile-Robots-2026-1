68ea1947aec4d1d8ffffe66a5c4dc404a47f3abb
RociorRomero0609 <rociofabiolaromerobernal@gmail.com>
2025-09-14 00:47:41 -0500

    tarea3

--- ros2_ws/src/navigation/map_augmenter/map_augmenter/map_inflater.py
+++ ros2_ws/src/navigation/map_augmenter/map_augmenter/map_inflater.py
@@ -13,21 +13,21 @@ from nav_msgs.msg import OccupancyGrid
 from nav_msgs.srv import GetMap
 import numpy
 
-FULL_NAME = "FULL NAME"
+FULL_NAME = "Romero Bernal Rocio Fabiola"
 
 class MapInflaterNode(Node):
     def get_inflated_map(self, static_map, inflation_cells):
         print("Inflating map by " + str(inflation_cells) + " cells")
         inflated = numpy.copy(static_map)
         [height, width] = static_map.shape
-        #
-        # TODO:
-        # Write the code necessary to inflate the obstacles in the map a radius
-        # given by 'inflation_cells' (expressed in number of cells)
-        # Map is given in 'static_map' as a bidimensional numpy array.
-        # Consider as occupied cells all cells with an occupation value greater than 50
-        #
-        
+        for i in range(len(static_map)):
+            for j in range(len(static_map[0])):
+                if(static_map[i,j] ==100):
+                    for k1 in range(-inflation_cells ,inflation_cells):
+                        for k2 in range(-inflation_cells ,inflation_cells):
+                            r= min(height-1, max(0, i+k1))
+                            c= min(width-1, max(0, j+k2))
+                            inflated[r,c] =100
         return inflated
 
     def callback_inflated_map(self, request, response):
