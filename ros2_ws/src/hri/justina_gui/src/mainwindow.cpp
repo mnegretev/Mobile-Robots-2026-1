ff55d07aae6e947b948ac33bda57481e736430de
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2025-09-17 20:41:21 -0600

    Correct gui for path planning

--- ros2_ws/src/hri/justina_gui/src/mainwindow.cpp
+++ ros2_ws/src/hri/justina_gui/src/mainwindow.cpp
@@ -25,6 +25,10 @@ MainWindow::MainWindow(QWidget *parent)
     QObject::connect(ui->btnTurnLeft, SIGNAL(released()), this, SLOT(btnTurnLeftReleased()));
     QObject::connect(ui->btnTurnRight, SIGNAL(pressed()), this, SLOT(btnTurnRightPressed()));
     QObject::connect(ui->btnTurnRight, SIGNAL(released()), this, SLOT(btnTurnRightReleased()));
+    
+    QObject::connect(ui->navTxtStartPose, SIGNAL(returnPressed()), this, SLOT(navBtnCalcPath_pressed()));
+    QObject::connect(ui->navTxtGoalPose, SIGNAL(returnPressed()), this, SLOT(navBtnCalcPath_pressed()));
+    QObject::connect(ui->navBtnCalcPath, SIGNAL(clicked()), this, SLOT(navBtnCalcPath_pressed()));
 }
 
 MainWindow::~MainWindow()
@@ -83,6 +87,62 @@ void MainWindow::btnTurnRightReleased()
 
 void MainWindow::navBtnCalcPath_pressed()
 {
+    float startX = 0;
+    float startY = 0;
+    float startA = 0;
+    float goalX = 0;
+    float goalY = 0;
+    float goalA = 0;
+    std::vector<std::string> parts;
+    
+    std::string str = this->ui->navTxtStartPose->text().toStdString();
+    boost::algorithm::to_lower(str);
+    boost::split(parts, str, boost::is_any_of(" ,\t\r\n"), boost::token_compress_on);
+    
+    // if(str.compare("") == 0 || str.compare("robot") == 0) //take robot pose as start position
+    // {
+    //     this->ui->navTxtStartPose->setText("Robot");
+    //     commNode->get_robot_pose(startX, startY, startA);
+    // }
+    // else
+    if(parts.size() >= 2) //Given data correspond to numbers
+    {
+        std::stringstream ssStartX(parts[0]);
+        std::stringstream ssStartY(parts[1]);
+        if(!(ssStartX >> startX) || !(ssStartY >> startY))
+        {
+            this->ui->navTxtStartPose->setText("Invalid format");
+            return;
+        }
+    }
+    else
+    {
+	this->ui->navTxtStartPose->setText("Invalid format");
+	return;
+    }
+
+    str = this->ui->navTxtGoalPose->text().toStdString();
+    boost::algorithm::to_lower(str);
+    boost::split(parts, str, boost::is_any_of(" ,\t\r\n"), boost::token_compress_on);
+    if(parts.size() >= 2)
+    {
+        std::stringstream ssGoalX(parts[0]);
+        std::stringstream ssGoalY(parts[1]);
+        if(!(ssGoalX >> goalX) || !(ssGoalY >> goalY))
+        {
+            this->ui->navTxtGoalPose->setText("Invalid format");
+            return;
+        }
+    }
+    else
+    {
+	this->ui->navTxtGoalPose->setText("Invalid format");
+	return;
+    }
+    nav_msgs::msg::Path path, smooth_path;
+    if(!commNode->call_plan_path(startX, startY, goalX, goalY, path))
+        return;
+    //commNode->call_smooth_path(path, smooth_path);
 }
 
 void MainWindow::navBtnExecPath_pressed()
