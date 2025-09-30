f3e2f719c5294d5de0a113ebf4bc8ccdb4d77138
Belmonte Gonzalez <melg4326@gmail.com>
2025-09-29 19:18:23 -0600

    Tarea Seis

--- ros2_ws/src/navigation/simple_move/simple_move/path_follower.py
+++ ros2_ws/src/navigation/simple_move/simple_move/path_follower.py
@@ -24,7 +24,7 @@ import math
 import numpy
 import time
 
-NAME = "FULL NAME"
+NAME = "Dania Melissa Belmonte González"
 
 SM_INIT = 0
 SM_WAIT_FOR_NEW_GOAL = 10
@@ -48,7 +48,10 @@ class PathFollowerNode(Node):
         # Remember to keep error angle in the interval (-pi,pi]
         # Return the tuple [v,w]
         #
-        
+        error_a=math.atan2((goal_y-robot_y),(goal_x-robot_x))-robot_a
+        error_a=(error_a+math.pi)%(2*math.pi)-math.pi
+        v=v_max*math.exp(-error_a*error_a/alpha)
+        w=w_max*(2/(1+math.exp(-error_a/beta))-1)
         return [v,w]
 
     def follow_path(self, path, alpha, beta, v_max, w_max, tol):
@@ -72,6 +75,18 @@ class PathFollowerNode(Node):
         #
         # END OF WHILE
         #
+        idx=0
+        Pg=path[idx]
+        Pr, theta=self.get_robot_pose()
+        P_global=path[-1]
+        while numpy.linalg.norm(P_global-Pr) > tol and rclpy.ok():
+            v,w = self.calculate_control(Pr[0],Pr[1],theta,Pg[0],Pg[1],alpha,beta,v_max,w_max)
+            
+            self.publish_and_save_data(Pr[0],Pr[1],theta,Pg[0],Pg[1],v,w)
+            Pr,theta=self.get_robot_pose()
+            if numpy.linalg.norm(Pg-Pr)<0.3:
+                idx=min(idx+1,len(path)-1)
+                Pg=path[idx]
         return
 
     def publish_and_save_data(self, robot_x, robot_y, robot_a, goal_x, goal_y, v,w):
