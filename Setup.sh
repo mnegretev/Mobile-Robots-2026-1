4ff1b05fd209491c811c3c85c775127be85c8f55
Mark The Magnificent <marco.negrete@ingenieria.unam.edu>
2025-11-17 21:08:05 -0600

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -1,1 +1,1 @@
-sudo apt install ros-jazzy-ros-gz ros-jazzy-gz-ros2-control ros-jazzy-moveit ros-jazzy-control-toolbox ros-jazzy-slam-toolbox ros-jazzy-nav2-amcl ros-jazzy-nav2-map-server
+sudo apt install ros-jazzy-ros-gz ros-jazzy-gz-ros2-control ros-jazzy-moveit ros-jazzy-control-toolbox ros-jazzy-slam-toolbox ros-jazzy-nav2-amcl ros-jazzy-nav2-map-server ros-jazzy-joint-trajectory-controller ros-jazzy-joint-state-broadcaster
